47fbf0a1b507a740a102d2f12104123a0c09616b
Ajmain Nishu <ajmainnishu@gmail.com>
2023-11-17 22:14:38 +0600

    feat: Dashboard added into privateroute

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -49,7 +49,7 @@ const router = createBrowserRouter([
   },
   {
     path: 'dashboard',
-    element: <Dashboard />,
+    element: <PrivateRoute><Dashboard /></PrivateRoute>,
     children: [
       {
         path: 'mycart',
